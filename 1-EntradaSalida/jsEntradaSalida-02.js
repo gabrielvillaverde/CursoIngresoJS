f273b10be4fd8c0f439778fd4a99812f023d8824
Gabriel Villaverde <gbr.villaverde@gmail.com>
2020-07-13 22:21:35 -0300

    Realicé los ejercicios 1, 2, 3, 4, 5 y 6 E/S

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,22 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	var nombre; // camel case
+
+	var nombreCompleto;
+
+	nombre = "Mario"; // string
+	nombre = 1; // number
+	nombre = true; // boolean
+	nombre = null; // null
+
+	var dato;
+
+	dato = prompt("Ingrese un dato?");
+
+	alert(dato);
+	
+
+
 }
 
